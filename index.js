39235872fb0069b755531899a4be6e5d86f5831d
Deepank Gupta <dgupta@linkedin.com>
2014-01-17 14:59:33 -0800

    Sample fetcher and skeleton code to integrate it.

--- index.js
+++ index.js
@@ -2,14 +2,73 @@
 
 const argv = require('optimist').argv;
 const _ = require('lodash');
+const traverse = require('traverse');
+const util = require('util')
+var config;
+
+
+
+function isPrimitive(type) {
+  return _.has(JAVASCRIPT_TO_AVRO_TYPE, type);
+}
+
+// TODO: Convert string to bytes, enum or fixed as well.
+var JAVASCRIPT_TO_AVRO_TYPE = {
+  'null': 'null',
+  'boolean': 'boolean',
+  'number': getNumberAvroType,
+  'string': getStringAvroType,
+}
+
+function getNumberAvroType(datum, key) {
+  if (datum % 1 === 0) {
+    return 'int';
+  } else {
+    return 'float';
+  }
+}
+
+function getStringAvroType(datum, key) {
+  var enumWhitelistedProperties = traverse.get(config, ['enum', 'whitelistedProperties']);
+  if (enumWhitelistedProperties && _.contains(enumWhitelistedProperties, key)) {
+    return 'enum';
+  } else {
+    return 'string';
+  }
+}
+
+function generateSchema(datum, key) {
+  console.log(datum, key);
+  var type = typeof datum;
+  if (isPrimitive(type)) {
+    var avroType = JAVASCRIPT_TO_AVRO_TYPE[type]
+    if (_.isFunction(avroType)) {
+      avroType = avroType(datum, key);
+    }
+    return {
+      name: key,
+      type: avroType,      
+      doc: ''
+    };
+  } else {
+    // TODO: Handle complex types
+    return _.map(datum, generateSchema);
+  }
+}
 
 function generate(data) {
   // TODO: Add the generation code.
-  console.log('Data: ', data);
+  var result = _.map(data, function(jsons, namespacePrefix) {
+    // TODO: Combine schemas from multiple samples.
+    return _.map(jsons, function(json) {
+      // TODO: Write these schemas out in the output directory.
+      return generateSchema(json, namespacePrefix);
+    });
+  });
+  console.log('Data: ', JSON.stringify(result));
 }
 
 function main() {
-  var config;
   try {
     config = require('./configs/' + argv.config);
   } catch (e) {
@@ -25,7 +84,6 @@ function main() {
   var fetcher;
   try {
     fetcher = require(config.fetcher);
-    console.log('Fetcher: ', fetcher);
   } catch (e) {
     console.log('Could not require ', config.fetcher);
     return -1;
